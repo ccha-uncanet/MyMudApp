3e94d6ad62ea9ea3403d8f521126bccc0c079c2d
ccha-uncanet <chuyij77chompoo@gmail.com>
2026-02-17 16:17:49 +0700

    Fix: Add RootComponents to Program.cs

--- MyMudApp.Client/Program.cs
+++ MyMudApp.Client/Program.cs
@@ -1,7 +1,16 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp =>
+    new HttpClient {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
 
 builder.Services.AddMudServices();
 
